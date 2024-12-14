baaa97b60fd3be3a170129df70fd1435808a2a3f
Ctrl-116 <2055454300@qq.com>
2024-12-14 19:50:01 +0800

    小改动：缩小行距

--- exercise/study/test1.c
+++ exercise/study/test1.c
@@ -1,85 +1,82 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdbool.h>
+
 // 检查数字是否满足条件的函数
-int jy1(int hw){
-    int z,g=sqrt(hw);
-    for(z=2;z<=g;z+=2) {  //检验判断号 即可
-        if(hw%z==0){
+int jy1(int hw) {
+    int z, g = sqrt(hw);
+    for (z = 2; z <= g; z += 2) {  // 检验判断号 即可
+        if (hw % z == 0) {
             goto end;
         }
     }
-    printf("%d\n",hw);
-    end:return(0);
+    printf("%d\n", hw);
+    end:
+    return(0);
 }
 
 // 另一个检查函数
-int jy2(int hw){
+int jy2(int hw) {
     int z;
-    for(z=3;z<hw;z+=2) {  //检验判断号 即可
-        if(hw%z==0){
+    for (z = 3; z < hw; z += 2) {  // 检验判断号 即可
+        if (hw % z == 0) {
             goto end;
         }
     }
-    printf("%d\n",hw);
-    end: return(0);
+    printf("%d\n", hw);
+    end:
+    return(0);
 }
 
 int main() {
-    int a,b,hw,d1,d2,d3,d4,z;
-    scanf("%d %d",&a,&b);
-    
+    int a, b, hw, d1, d2, d3, d4, z;
+    scanf("%d %d", &a, &b);
     // 一位数检查
-    for (d1 = 5; d1 <= 9; d1+=2){
-        hw=d1;
-        if(hw>=a &&hw<=b){
+    for (d1 = 5; d1 <= 9; d1 += 2) {
+        hw = d1;
+        if (hw >= a && hw <= b) {
             jy2(hw);
         }
     }
-    
     // 两位数检查
-    for (d1 = 1; d1<=9; d1+=2){
-        hw=d1*11;
-        if(hw>=a &&hw<=b){
+    for (d1 = 1; d1 <= 9; d1 += 2) {
+        hw = d1 * 11;
+        if (hw >= a && hw <= b) {
             jy2(hw);
         }
     }
-    
     // 三位数检查
-    for (d1 = 1; d1 <= 9; d1+=2){
-        for (d2 = 0; d2 <= 9; d2++){
-            hw=d1*100+d2*10+d1;
-            if(hw>=a &&hw<=b){
+    for (d1 = 1; d1 <= 9; d1 += 2) {
+        for (d2 = 0; d2 <= 9; d2++) {
+            hw = d1 * 100 + d2 * 10 + d1;
+            if (hw >= a && hw <= b) {
                 jy2(hw);
             }
         }
     }
-    
     // 五位数检查
-    for (d1 = 1; d1 <= 9; d1+=2) {
+    for (d1 = 1; d1 <= 9; d1 += 2) {
         for (d2 = 0; d2 <= 9; d2++) {
             for (d3 = 0; d3 <= 9; d3++) {
-                hw= 10000*d1 + 1000*d2 +100*d3 + 10*d2 + d1;
-                if(hw>=a &&hw<=b){
+                hw = 10000 * d1 + 1000 * d2 + 100 * d3 + 10 * d2 + d1;
+                if (hw >= a && hw <= b) {
                     jy1(hw);
                 }
             }
         }
     }
-    
     // 七位数检查
-    for (d1 = 1; d1 <= 9; d1+=2) {
+    for (d1 = 1; d1 <= 9; d1 += 2) {
         for (d2 = 0; d2 <= 9; d2++) {
             for (d3 = 0; d3 <= 9; d3++) {
-                for (d4 = 0; d4 <= 9; d4++){
-                    hw= 1000000*d1 + 100000*d2 +10000*d3 + 1000*d4+d3*100 +d2*10+ d1;
-                    if(hw>=a &&hw<=b){
+                for (d4 = 0; d4 <= 9; d4++) {
+                    hw = 1000000 * d1 + 100000 * d2 + 10000 * d3 + 1000 * d4 + d3 * 100 + d2 * 10 + d1;
+                    if (hw >= a && hw <= b) {
                         jy1(hw);
                     }
                 }
             }
         }
     }
-    
     return 0;
 }
