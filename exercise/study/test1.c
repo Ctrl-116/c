5dfc5db01bd8388f678c3bd408bd374b77018fdb
Ctrl-116 <2055454300@qq.com>
2024-12-14 18:48:56 +0800

    提交回文质数AC代码

--- exercise/study/test1.c
+++ exercise/study/test1.c
@@ -1,150 +1,85 @@
-#include<stdio.h>
+#include <math.h>
+#include <stdio.h>
 #include <stdbool.h>
-
-bool isPrime(int x)
-{
-	bool ret = true;
-	if(x == 1)ret = false;
-	else if((x != 2 && x != 3) && (x % 2 == 0 || x % 3 == 0)){
-		ret = false;
-	}
-	if(ret){
-		for(int i = 5; i * i <= x; i += 6){
-			if(x % i == 0 || x % (i + 2) == 0){
-				ret = false;
-			}
-		}
-	}
-	return ret;
-}
-
-int condition(int x, int a, int b){
-    int ret = 0;
-    if(x >= a && x <= b){
-        ret = 1;
+// 检查数字是否满足条件的函数
+int jy1(int hw){
+    int z,g=sqrt(hw);
+    for(z=2;z<=g;z+=2) {  //检验判断号 即可
+        if(hw%z==0){
+            goto end;
+        }
     }
-    return ret;
+    printf("%d\n",hw);
+    end:return(0);
 }
-int main()
-{
-    int a, b;
-    scanf("%d %d", &a, &b);
-	int palindrome;
-	int d1, d2, d3, d4, d5;
-	
-	 // 只有奇数才会是素数
-	//1
 
-	for(d1 = 1; d1 <= 9; d1 += 2){
-		palindrome = d1;
-        if(isPrime(palindrome) && condition(palindrome, a, b)){
-            printf("%d\n", palindrome);
+// 另一个检查函数
+int jy2(int hw){
+    int z;
+    for(z=3;z<hw;z+=2) {  //检验判断号 即可
+        if(hw%z==0){
+            goto end;
         }
-	} 
-	
-	//2
-	for(d1 = 1; d1 <= 9; d1 += 2){
-		palindrome = d1 * 10 + d1;
-        if(isPrime(palindrome) && condition(palindrome, a, b)){
-            printf("%d\n", palindrome);
-        }
+    }
+    printf("%d\n",hw);
+    end: return(0);
+}
 
-	}
-	
-	//3
-	for(d1 = 1; d1 <= 9; d1 += 2){
-		for(d2 = 0; d2 <= 9; d2 ++){
-			palindrome = d1 * 100 + d2 * 10 + d1;
-            if(isPrime(palindrome) && condition(palindrome, a, b)){
-                printf("%d\n", palindrome);
+int main() {
+    int a,b,hw,d1,d2,d3,d4,z;
+    scanf("%d %d",&a,&b);
+    
+    // 一位数检查
+    for (d1 = 5; d1 <= 9; d1+=2){
+        hw=d1;
+        if(hw>=a &&hw<=b){
+            jy2(hw);
+        }
+    }
+    
+    // 两位数检查
+    for (d1 = 1; d1<=9; d1+=2){
+        hw=d1*11;
+        if(hw>=a &&hw<=b){
+            jy2(hw);
+        }
+    }
+    
+    // 三位数检查
+    for (d1 = 1; d1 <= 9; d1+=2){
+        for (d2 = 0; d2 <= 9; d2++){
+            hw=d1*100+d2*10+d1;
+            if(hw>=a &&hw<=b){
+                jy2(hw);
             }
-		}
-	}
-	
-	//4
-	for(d1 = 1; d1 <= 9; d1 += 2){
-		for(d2 = 0; d2 <= 9; d2++){
-			palindrome = d1 * 1000 + d2 * 100 + d2 * 10 + d1;
-            if(isPrime(palindrome) && condition(palindrome, a, b)   ){
-                printf("%d\n", palindrome);
+        }
+    }
+    
+    // 五位数检查
+    for (d1 = 1; d1 <= 9; d1+=2) {
+        for (d2 = 0; d2 <= 9; d2++) {
+            for (d3 = 0; d3 <= 9; d3++) {
+                hw= 10000*d1 + 1000*d2 +100*d3 + 10*d2 + d1;
+                if(hw>=a &&hw<=b){
+                    jy1(hw);
+                }
             }
-		}
-	}
-	
-	//5
-	for(d1 = 1; d1 <= 9; d1 += 2){
-		for(d2 = 0; d2 <= 9; d2++){
-			for(d3 = 0; d3 <= 9; d3++){
-				palindrome = d1 * 10000 + d2 * 1000 + d3 * 100 + d2 * 10 + d1;
-                if(isPrime(palindrome) && condition(palindrome, a, b)){
-                    printf("%d\n", palindrome);
-                }
-			}
-		}
-	}
-	
-	//6
-	for(d1 = 0; d1 <= 9; d1 += 2){
-		for(d2 = 0; d2 <= 9; d2++){
-			for(d3 = 0; d3 <= 9; d3++){
-                palindrome = d1 * 100000 + d2 * 10000 + d3 * 1000 + d3 * 100 + d2 * 10 + d1;
-                if(isPrime(palindrome) && condition(palindrome, a, b)){
-                    printf("%d\n", palindrome);
-                }
-			}
-		}
-	}
-	
-	//7
-    for(d1 = 1; d1 <= 9; d1 += 2){
-        for(d2 = 0; d2 <= 9; d2++){
-            for(d3 = 0; d3 <= 9; d3++){
-                for(d4 = 0; d4 <= 9; d4++){
-                    palindrome = d1 * 1000000 + d2 * 100000 + d3 * 10000 + d4 * 1000 + d4 * 100 + d3 * 10 + d2 * 1 + d1;
-                    if(isPrime(palindrome) && condition(palindrome, a, b)){
-                        printf("%d\n", palindrome);
+        }
+    }
+    
+    // 七位数检查
+    for (d1 = 1; d1 <= 9; d1+=2) {
+        for (d2 = 0; d2 <= 9; d2++) {
+            for (d3 = 0; d3 <= 9; d3++) {
+                for (d4 = 0; d4 <= 9; d4++){
+                    hw= 1000000*d1 + 100000*d2 +10000*d3 + 1000*d4+d3*100 +d2*10+ d1;
+                    if(hw>=a &&hw<=b){
+                        jy1(hw);
                     }
                 }
             }
         }
     }
-
-    //8
-    for(d1 = 1; d1 <= 9; d1 += 2){
-        for(d2 = 0; d2 <= 9; d2++){
-            for(d3 = 0; d3 <= 9; d3++){
-                for(d4 = 0; d4 <= 9; d4++){
-                    palindrome = d1 * 10000000 + d2 * 1000000 + d3 * 100000 + d4 * 10000 + d4 * 1000 + d3 * 100 + d2 * 10 + d1;
-                    if(isPrime(palindrome) && condition(palindrome, a, b)){
-                        printf("%d\n", palindrome);
-                    }
-                }
-            }
-        }
-    }
-	//9
-	for (d1 = 1; d1 <= 9; d1 += 2) {   
-		for (d2 = 0; d2 <= 9; d2++) {
-			for (d3 = 0; d3 <= 9; d3++) {
-				for(d4 = 0; d4 <= 9; d4++){
-					for(d5 = 0; d5 <= 9; d5++){
-						palindrome = d1 * 100000000 + d2 * 10000000 + d3 * 1000000 + d4 * 100000  + d5 * 10000 
-						+ d4 * 1000 + d3 * 100 + d2 * 10 + d1;
-                        if(isPrime(palindrome) && condition(palindrome, a, b)   ){
-                            printf("%d\n", palindrome);
-                        }
-					}
-				}
-			}
-		}
- 	}
- 	
-	return 0;
+    
+    return 0;
 }
-
-
-
-
-
-
-
