d59a3cfea26501b2245441dc549b553392aa3d00
Ctrl-116 <2055454300@qq.com>
2024-12-15 11:36:39 +0800

    回文AC

--- exercise/study/test1.c
+++ exercise/study/test1.c
@@ -1,82 +1,84 @@
-#include <math.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <math.h>
 
-// 检查数字是否满足条件的函数
-int jy1(int hw) {
-    int z, g = sqrt(hw);
-    for (z = 2; z <= g; z += 2) {  // 检验判断号 即可
-        if (hw % z == 0) {
-            goto end;
-        }
-    }
-    printf("%d\n", hw);
-    end:
-    return(0);
+int pd1(int hw)
+{
+	for(int i = 3; i < hw; i++){
+		if(hw % i == 0){
+			return 0;
+		}
+	}
+	printf("%d\n", hw);
+	return 0;
 }
 
-// 另一个检查函数
-int jy2(int hw) {
-    int z;
-    for (z = 3; z < hw; z += 2) {  // 检验判断号 即可
-        if (hw % z == 0) {
-            goto end;
-        }
-    }
-    printf("%d\n", hw);
-    end:
-    return(0);
+int pd2(int hw)
+{
+	int t = sqrt(hw);
+	for(int i = 3; i <= t; i += 2){
+		if(hw % i == 0){
+			return 0;
+		}
+	}
+	printf("%d\n", hw);
+	return 0;
 }
 
-int main() {
-    int a, b, hw, d1, d2, d3, d4, z;
-    scanf("%d %d", &a, &b);
-    // 一位数检查
-    for (d1 = 5; d1 <= 9; d1 += 2) {
-        hw = d1;
-        if (hw >= a && hw <= b) {
-            jy2(hw);
-        }
-    }
-    // 两位数检查
-    for (d1 = 1; d1 <= 9; d1 += 2) {
-        hw = d1 * 11;
-        if (hw >= a && hw <= b) {
-            jy2(hw);
-        }
-    }
-    // 三位数检查
-    for (d1 = 1; d1 <= 9; d1 += 2) {
-        for (d2 = 0; d2 <= 9; d2++) {
-            hw = d1 * 100 + d2 * 10 + d1;
-            if (hw >= a && hw <= b) {
-                jy2(hw);
-            }
-        }
-    }
-    // 五位数检查
-    for (d1 = 1; d1 <= 9; d1 += 2) {
-        for (d2 = 0; d2 <= 9; d2++) {
-            for (d3 = 0; d3 <= 9; d3++) {
-                hw = 10000 * d1 + 1000 * d2 + 100 * d3 + 10 * d2 + d1;
-                if (hw >= a && hw <= b) {
-                    jy1(hw);
-                }
-            }
-        }
-    }
-    // 七位数检查
-    for (d1 = 1; d1 <= 9; d1 += 2) {
-        for (d2 = 0; d2 <= 9; d2++) {
-            for (d3 = 0; d3 <= 9; d3++) {
-                for (d4 = 0; d4 <= 9; d4++) {
-                    hw = 1000000 * d1 + 100000 * d2 + 10000 * d3 + 1000 * d4 + d3 * 100 + d2 * 10 + d1;
-                    if (hw >= a && hw <= b) {
-                        jy1(hw);
-                    }
-                }
-            }
-        }
-    }
-    return 0;
-}
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	
+	int hw, d1, d2, d3, d4;
+	
+	//判断1位数
+	for(d1 = 5; d1 <= 9; d1 += 2){
+		hw = d1;
+		if(hw >= a && hw <= b){
+			pd1(hw);
+		}
+	} 
+	//判断两位数
+	for(d1 = 1; d1 <= 9; d1 += 2){
+		hw = d1 * 10 + d1;
+		if(hw >= a && hw <= b){
+			pd1(hw);
+		}
+	} 
+	//判断三位数
+	for(d1 = 1; d1 <= 9; d1 += 2) {
+		for(d2 = 0; d2 <= 9; d2++){
+			hw= d1 * 100 + d2 * 10 + d1;
+			if(hw >= a && hw <= b){
+				pd1(hw);
+			}
+		}
+	}
+	 
+	//四位数及四位数之后的偶数位回文数都含有11这个因数，都不是质数 
+	//判断五位数 
+	for(d1 = 1; d1 <= 9; d1 += 2){
+		for(d2 = 0; d2 <= 9; d2++){
+			for(d3 = 0; d3 <= 9; d3++){
+				hw = d1 * 10000 + d2 * 1000 + d3 * 100 + d2 * 10 + d1;
+				if(hw >= a && hw <= b){
+					pd2(hw);
+				} 
+			}
+		}
+	} 
+	//判断七位数
+	for(d1 = 1; d1 <= 9; d1 += 2){
+		for(d2 = 0; d2 <= 9; d2++){
+			for(d3 = 0; d3 <= 9; d3++){
+				for(d4 = 0; d4 <= 9; d4++){
+					hw = d1 * 1000000 + d2 * 100000 + d3 * 10000 + d4 * 1000 + d3 * 100 + d2 * 10 + d1;
+					if(hw >= a && hw <= b){
+						pd2(hw);
+					}
+				}
+			}
+		}
+	} 
+	return 0;
+ } 
